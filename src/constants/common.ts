6938b30315354fd89b0757d78ab4d6366d44b3c7
Samouczek <795kasia@gmail.com>
2022-12-03 21:24:23 +0100

    add text constants

---
+++ src/constants/common.ts
@@ -0,0 +1,5 @@
+export const BUTTON = {
+	REGISTRATION_FORM: 'formularz rejestracyjny',
+	NEXT_PROFILES: 'next profiles',
+	SAVE: 'zapisz',
+};
---
+++ src/constants/components.ts
@@ -0,0 +1,15 @@
+export const PERSON_PERFORMING_TASK = 'imie i nazwisko';
+
+export const REGISTRATION = {
+	HEADING: 'FORMULARZ REJESTRACYJNY',
+	LOGIN_LABEL: 'Login:',
+	PASSWORD_LABEL: 'Hasło:',
+	EMAIL_LABEL: 'E-mail:',
+	PHONE_NUMBER: 'Numer telefonu:',
+	LOGIN_HELPER_TEXT: 'Login jest wymagany',
+	PASSWORD_HELPER_TEXT: 'Hasło jest wymagane',
+	EMAIL_HELPER_TEXT: 'Nieprawidłowy format adresu e-mail',
+	PHONE_HELPER_TEXT: 'Nieprawidłowy numer telefonu',
+	ACCEPT_REGULATIONS: 'Akceptuję Regulamin',
+	ACCEPT_REGULATIONS_HELPER_TEXT: 'Wymagana akceptacja regulaminu',
+};
