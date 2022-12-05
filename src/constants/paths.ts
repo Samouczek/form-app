1dabd318ed882c746ff4c2c71b53784067858dcf
Samouczek <795kasia@gmail.com>
2022-12-05 09:42:51 +0100

    path changed

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,5 +1,5 @@
-export const PERSON_PAGE = '/';
-export const REGISTRATION_FORM_PAGE = 'registration';
+export const PERSON_PAGE = '/form-app';
+export const REGISTRATION_FORM_PAGE = '/form-app/registration';
 export const IMAGE_GENERATING_LINK = 'https://picsum.photos/534/383';
 export const ICON_PATH = '../../../assets/icons/profile.svg';
 export const BASIC_DATA_PATH = 'https://swapi.py4e.com/api/people/';
